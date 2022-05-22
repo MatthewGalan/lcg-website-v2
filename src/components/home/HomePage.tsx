85bddf59bc172e22cfced66b59402880835f9e3f
MatthewGalan <mattg54321@gmail.com>
2022-05-22 15:04:44 -0400

    first attempt at logo wrap bug

--- src/components/home/HomePage.tsx
+++ src/components/home/HomePage.tsx
@@ -8,7 +8,7 @@ import LoadingSpinner from "../common/LoadingSpinner";
 import AboutMe from "./AboutMe";
 
 const StyledLogo = styled.div`
-  margin: 64px auto 16px auto;
+  margin: 64px 0 16px 0;
   text-align: center;
 
   .lori {
